b9c8d6aeb9d0ce099a2cf1b98a0be2f375299f51
Nicolás Bacquet <nibata@gmail.com>
2023-02-18 23:26:48 -0300

    cambios sencillos para seguir patron de app factory

--- flask-apps/app.py
+++ flask-apps/app.py
@@ -1,5 +1,7 @@
-from apps.app_test import app as app_test
+from apps.app_test import create_app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+app_test = create_app()
 
 # En el caso de que quisiera agregar más aplicaciones debería realizarlo acá, asignando 
 # el path y la variable app de cada aplicación que se encuentra en el __init__.py
